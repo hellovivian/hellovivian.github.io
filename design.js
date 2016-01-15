74764e0ac41257b4bca8d09103f1875028346b6d
hellovivian <vivian.l.061@gmail.com>
2016-01-15 01:00:45 -0800

    Added design nav hover

--- design.js
+++ design.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+      $("#nav").hover(function() {
+      if ($("#nav").hasClass("navactive")) {
+         $("#navtxt1").fadeOut();
+         $("#navtxt2").fadeOut();
+         $("#navtxt3").fadeOut();
+         $("#nav").removeClass("navactive");
+      } else {
+      $("#navtxt1").fadeIn();
+      $("#navtxt2").fadeIn();
+      $("#navtxt3").fadeIn();
+      $("#nav").addClass("navactive");
+         
+      }
+   });
+   
 	var t = setInterval(function(){
 		$("#carousel ul").animate({marginLeft:-200},1000,function(){
 			$(this).find("li:last").after($(this).find("li:first"));
