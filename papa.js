fa409fc4db036793073719abb4db618cbf583521
hellovivian <vivian.l.061@gmail.com>
2016-07-05 21:58:40 -0700

    Happy bday dad

---
+++ papa.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+   
+   
+      $("#twentyone").hover(function() {
+      if ($("#text8").hasClass("textactive")) {
+         $("#text8").hide();
+         $("#text8").removeClass("textactive");
+      } else {
+      $("#text8").show();
+      $("#text8").addClass("textactive"); 
+      }
+   });
+   
+      $("#thirteen").hover(function() {
+      if ($("#text2").hasClass("textactive")) {
+         $("#text2").hide();
+         $("#text2").removeClass("textactive");
+      } else {
+      $("#text2").show();
+      $("#text2").addClass("textactive"); 
+      }
+   });
+   
+         $("#eight").hover(function() {
+      if ($("#text3").hasClass("textactive")) {
+         $("#text3").hide();
+         $("#text3").removeClass("textactive");
+      } else {
+      $("#text3").show();
+      $("#text3").addClass("textactive"); 
+      }
+   });
+   
+   $("#five").hover(function() {
+      if ($("#text4").hasClass("textactive")) {
+         $("#text4").hide();
+         $("#text4").removeClass("textactive");
+      } else {
+      $("#text4").show();
+      $("#text4").addClass("textactive"); 
+      }
+   });
+   
+      $("#three").hover(function() {
+      if ($("#text5").hasClass("textactive")) {
+         $("#text5").hide();
+         $("#text5").removeClass("textactive");
+      } else {
+      $("#text5").show();
+      $("#text5").addClass("textactive"); 
+      }
+   });
+   
+          $("#two").hover(function() {
+      if ($("#text7").hasClass("textactive")) {
+         $("#text7").hide();
+         $("#text7").removeClass("textactive");
+      } else {
+      $("#text7").show();
+      $("#text7").addClass("textactive"); 
+      }
+   });
+   
+         $("#not1").hover(function() {
+      if ($("#text6").hasClass("textactive")) {
+         $("#text6").hide();
+         $("#text6").removeClass("textactive");
+      } else {
+      $("#text6").show();
+      $("#text6").addClass("textactive"); 
+      }
+   });
+   
+   
+            $("#one").hover(function() {
+      if ($("#text9").hasClass("textactive")) {
+         $("#text9").hide();
+         $("#text9").removeClass("textactive");
+      } else {
+      $("#text9").show();
+      $("#text9").addClass("textactive"); 
+      }
+   });
+   
+   
+});
