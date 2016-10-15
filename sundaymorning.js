677b2ead7697165d005223739be93b811d38d888
Vivian Liu <v.liu@berkeley.edu>
2016-10-15 14:43:13 -0700

    Revert "Revert "taking away circles""
    This reverts commit f0826e4a8824dbd82382166d9f0134870b30ae9b.

---
+++ sundaymorning.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+      $("#name").hover(function() {
+      if ($("#nav").hasClass("navactive")) {
+         Window.alert("hi");
+      } else {
+      
+         
+      }
+   });
+   
+	
+
+});
