c2cef60a8b19305648826e3a57723c19eabc1cd6
Vivian Liu <v.liu@berkeley.edu>
2016-12-26 11:43:06 -0800

    Implemented hovering board.

--- hellome2/sundaymorning.js
+++ hellome2/sundaymorning.js
@@ -9,24 +9,381 @@ $(document).ready(function(){
            $("#bike").css("transition-duration","2s");
          
    
-         $("#balloon").css("transform","translate(-200px,-50px");
-         $("#balloon").css("transition-duration","3s");
-        
 
       });
    
    
       $("#hobbies").click(function() {
-                          
+         $("#projectscontainer").fadeIn();
+         $("#sidecontainer").fadeIn();
          $("#desc").hide();
          $("#bike2").hide();
          $("body").addClass("hobbiesactive");
-
-
-         $("#bike").css("transform","translate(0px,500px)");
-         $("#bike").css("transition-duration","2s");
+ 
+         $("#sky").css("transform","translate(0px,75px)");
+         $("#sky").css("transition-duration","2s");
 
 
       });
+   
+     $( "#neg15" ).hover(function() {
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+   
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+      $("#neg15text").show();
+      $(this).addClass("rowactive");
+   }        
+ });
+   
+   
+   $( "#neg14" ).hover(function() {
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+      $("#neg15text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+      $("#neg14text").show();
+      $(this).addClass("rowactive");
+   }        
+ });
+        
+  $( "#neg13" ).hover(function() {
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+      $("#neg13text").show();
+      $(this).addClass("rowactive");
+   }        
+ });
+   
+  $( "#neg12" ).hover(function() {
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+      $("#neg12text").show();
+      $(this).addClass("rowactive");
+   }        
+ });
+    
+   
+$( "#neg11" ).hover(function() {
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+      $("#neg11text").show();
+      $(this).addClass("rowactive");
+   }        
+ });
+   
+ $( "#neg10" ).hover(function() {
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+      $("#neg10text").show();
+      $(this).addClass("rowactive");
+   }        
+ });
+   
+ $( "#neg9" ).hover(function() {
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+      $("#neg9text").show();
+      $(this).addClass("rowactive");
+   }        
+ });
+   
+$( "#neg8" ).hover(function() {
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+      $("#neg8text").show();
+      $(this).addClass("rowactive");
+   }        
+ });
+   
+   
+ $( "#neg7" ).hover(function() {
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+      $("#neg7text").show();
+      $(this).addClass("rowactive");
+   }        
+ });
+   
+   
+   $( "#neg6" ).hover(function() {
+ 
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+       $("#neg6text").show();
+       $(this).addClass("rowactive");
+   }        
+ });
+      
+   $( "#neg5" ).hover(function() {
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+       $("#neg5text").show();
+       $(this).addClass("rowactive");
+   }        
+ });
+   
+   
+   $( "#neg4" ).hover(function() {
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+       $("#neg4text").show();
+       $(this).addClass("rowactive");
+   }        
+ });
+   
+   
+   $( "#neg3" ).hover(function() {
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg2text").hide();
+      $("#neg1text").hide();
+       $("#neg3text").show();
+       $(this).addClass("rowactive");
+   }        
+ 
+ });
+   
+      $( "#neg2" ).hover(function() {
+ 
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
 
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg1text").hide();
+       $("#neg2text").show();
+       $(this).addClass("rowactive");
+   }        
+ 
+ });
+   
+      $( "#neg1" ).hover(function() {
+ 
+   if ($(this).hasClass("rowactive")) {
+         $(this).removeClass("rowactive");
+
+   } else {
+      $("#neg15text").hide();
+      $("#neg14text").hide();
+      $("#neg13text").hide();
+      $("#neg12text").hide();
+      $("#neg11text").hide();
+      $("#neg10text").hide();
+      $("#neg9text").hide();
+      $("#neg8text").hide();
+      $("#neg7text").hide();
+      $("#neg6text").hide();
+      $("#neg5text").hide();
+      $("#neg4text").hide();
+      $("#neg3text").hide();
+      $("#neg2text").hide();
+       $("#neg1text").show();
+       $(this).addClass("rowactive");
+   }        
+ 
+ });
+   
 });
