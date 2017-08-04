721d955ae4dd3517c4a3a9375f3ff8504ff45c34
hellovivian <v.liu@berkeley.edu>
2017-08-04 13:44:05 -0700

    many changes to look and feel and javascript functionality

--- AgentsAuthoringTool/Example.js
+++ AgentsAuthoringTool/Example.js
@@ -1,41 +1,38 @@
-
-   var s = new sigma('container');
-   var lastNode;
-
+var s = new sigma('container');
+var lastNode;
     
-    s.graph.addNode({
-    
-      id: 'n0',
-      label: 'ActiveMQ',
-      x: 0,
-      y: 0,
-      size: 1,
-      color: '#f2552c'
-    }).addNode({
-      // Main attributes:
-      id: 'n1',
-      label: 'Default GIFT ActiveMQ Gateway',
-      // Display attributes:
-      x: 1,
-      y: 1,
-      size: 1,
-      color: '#004c8e'
-    }).addEdge({
-      id: 'e0',
-      // Reference extremities:
-      source: 'n0',
-      target: 'n1',
-       color: '#96dfe4'
-      
-    });
 
-    // Finally, let's ask our sigma instance to refresh:
+//Default gateway graph
+s.graph.addNode({
+ 
+   id: 'ActiveMQ',
+   label: 'ActiveMQ',
+   x: 0,
+   y: 0,
+   size: 1,
+   color: '#004c8e'
+ }).addNode({
+
+   id: 'Default GIFT ActiveMQ Gateway',
+   label: 'Default GIFT ActiveMQ Gateway',
+   x: 1,
+   y: 1,
+   size: 1,
+   color: '#004c8e'
+ }).addEdge({
+   id: 'e0',
+   source: 'ActiveMQ',
+   target: 'Default GIFT ActiveMQ Gateway',
+   color: '#83C8C6'    
+});
+
     s.refresh();
 
+   s.settings('zoomingRatio', 1);
 
-    function addNodes() {
-       var newNodeName = grabTextVal();
-         s.graph.addNode({id: newNodeName, label:newNodeName,x: 2, y:2,size: 1, color:'#00f'}).addEdge({id:'e1',source:'n1', target:newNodeName});
+function addNodes() {
+       var newNodeName = grabAddNodeVal();
+         s.graph.addNode({id: newNodeName, label:newNodeName,x: 2, y:2,size: 1, color:'#CD4A42'}).addEdge({id:'e1',source:'Default GIFT ActiveMQ Gateway', target:newNodeName, color:'#83C8C6'});
          s.refresh();
        
        //So we can add to the next node
@@ -44,20 +41,24 @@
       }
 
 
-function grabTextVal() {
-   var newNodeName = $('#nodeName').val();
-   //alert(String(newNodeName));
+function grabAddNodeVal() {
+   var newNodeName = $('#nodeAddName').val();
+
    return String(newNodeName);
 }
 
- $(document).ready(function() {
+function grabTextVal() {
+   var newNodeName = $('#nodeToRemove').val();
+
+   return String(newNodeName);
+}
+
+
     
-  
-    
-      function removeNodes() {
-         s.graph.dropNode('hi');
+function removeNodes() {
+          var lostNodeName = grabTextVal();
+         s.graph.dropNode(lostNodeName);
          s.refresh();
          alert("done");
          }
     
-        });
