412c5887a86f05bdcb5e8cdae309081909296957
hellovivian <v.liu@berkeley.edu>
2017-12-04 19:44:56 -0800

    can i add text now??

--- PgTemplate.js
+++ PgTemplate.js
@@ -4,12 +4,25 @@
          var modal = document.getElementById('modal');
         var notModal = document.getElementById('notModal');
           if ($("#modal").is(":visible")) {
+             
+           var children = modal.getElementsByTagName('img');
+               for  ( num = 0; num < modal.getElementsByTagName('img').length; num++) {
+                  children[num].remove();
+               }
+              
+    
+             $(strId+ "story").hide();
             $("#modal").hide();
              $("#notModal").hide();
           } else {
-               modal.innerHTML = "";
+               
                $("#modal").show();
              $("#notModal").show();
-               var clone = $(strId).clone().appendTo(modal);    
+
+               var clone = $(strId).clone().appendTo(modal); 
+               $(strId+ "story").show();
+
+    
+            
         }
       }
