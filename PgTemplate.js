90256b194ed53c86548c96de9c4329441507a6cc
hellovivian <v.liu@berkeley.edu>
2018-05-16 09:32:50 -0700

    dropdown now functional

--- PgTemplate.js
+++ PgTemplate.js
@@ -14,6 +14,34 @@ function span_onclick() {
    
 }
 
+//upon selecting from the drop down we show only container cards that have the chosen class
+function selection_onclick(choice) {
+
+   var containers = document.getElementsByClassName('container');
+   var containersArr = [].slice.call(containers);
+  
+   //   hide all at first
+   for (childNum = 0; childNum < containersArr.length; childNum++) {
+      var childName = containersArr[childNum].getElementsByTagName('img')[0].id;
+      $("#" + String(childName)).hide(); 
+      }
+   
+   //show the ones with the chosen class
+   var classElem = document.getElementsByClassName(String(choice));
+   var classElemArr = [].slice.call(classElem);
+
+   var classElemNamesArr = [];
+   
+   for (childNum = 0; childNum < classElemArr.length; childNum++) {
+      var childName = classElemArr[childNum].getElementsByTagName('img')[0].id;
+//      alert(childName);
+      $("#" + String(childName)).show(); 
+   }
+
+   
+   
+}
+
 function image_onclick(id) {
     var modal = document.getElementById('modal');
    if ($("#modal").children("img").length > 0) {
