6d2b046f4ee41fd800bad00f34277f99dd3fe4db
Vivian <v.liu@berkeley.edu>
2024-05-11 06:02:11 -0400

    Adding blog

--- src/BlogPage.js
+++ src/BlogPage.js
@@ -1,6 +1,10 @@
 // AboutPage.js
 import './App.css';
 // import './Profile.css';
+import BlogPost from './components/BlogPost';
+
+import blogData from './data/blogData.js';
+import AliceCarousel from 'react-alice-carousel';
 
 
 import { styled } from '@mui/material/styles';
@@ -9,7 +13,57 @@ import React from 'react';
 import Navigation from './components/Navigation';
 import Video from './components/Video';
 import ReactPlayer from 'react-player'
+import 'react-alice-carousel/lib/alice-carousel.css';
 
+const responsive = {
+  0: { items: 1 },
+  256: { items: 2 },
+  512: { items: 3 },
+  768: { items: 4 },
+  1024: { items: 5 },
+};
+
+const items = [
+  <div className="item" data-value="1">
+
+<video src="./disco_videos/lastcarnival.mp4" controls={true}></video>
+
+
+  
+   </div>,
+     <div className="item" data-value="1">
+
+{/* <video src="./disco_videos/lana.mp4" controls={true}></video> */}
+ 
+  </div>,
+  <div className="item" data-value="2">
+
+{/* <video src="./disco_videos/taylor_mirrorball.mp4" controls={true}></video> */}
+
+      
+      
+  </div>,
+  <div className="item" data-value="3">
+              {/* <img width="256px" src="./logomotion_gifs/warrior1.gif"></img> */}
+
+
+  </div>
+
+];
+
+const Carousel = () => (
+
+    
+
+
+  <AliceCarousel
+
+      mouseTracking
+      items={items}
+      responsive={responsive}
+      controlsStrategy="alternate"
+  />
+);
 
 const BlogPage = () => {
   return (
@@ -24,7 +78,18 @@ const BlogPage = () => {
     <div>
     <p class="about_text">Under construction, please standby. :) </p>
 
-    <div >
+
+   
+
+        {blogData.map((project) => (
+
+
+      
+<BlogPost projectname={project.title} date={project.date} authors={project.authors} projectpage={project.links.pg} codelink={project.links.codelink} video={project.links.video} demolink={project.links.demolink} paperlink={project.links.paperlink} venue={project.venue} imgsrc={project.links.img_src}/>
+
+
+))}
+ {/* <div >
             <ReactPlayer
             className='react-player fixed-bottom'
             url="./disco_videos/lastcarnival.mp4"
@@ -36,7 +101,7 @@ const BlogPage = () => {
 
     
 
-        </div>
+        </div> */}
 
 
     
@@ -61,7 +126,9 @@ const BlogPage = () => {
     </div>
 
       
-
+<h3>Disco Playlist</h3>
+<hr></hr>
+    <Carousel></Carousel>
 
      
     </div>
