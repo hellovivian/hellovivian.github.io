ae0e8d61ec8cb8761b96bf9936c28378747ce372
Vivian <v.liu@berkeley.edu>
2025-02-24 11:45:29 -0500

    mobile responsive about page

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,121 +1,116 @@
 // AboutPage.js
 import './App.css';
-// import './Profile.css';
 
-
-import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import React from 'react';
 import Navigation from './components/Navigation';
-import { Link } from 'react-router-dom';
+
 
 
 const AboutPage = () => {
   return (
     <div class="container" >
-        
-     <Navigation />
+
+      <Navigation />
 
       <h1>About</h1>
-      
-    <div className="profile-container">
-      <div class="split-horizontal">
+
+      <div className="profile-container">
+        <div class="split-horizontal">
+          <div>
+
+            <p class="about_text">I'm currently a fifth year Computer Science PhD Student at Columbia University and a student researcher at Google Deepmind. My PhD was supported by a National Science Foundation Graduate Research Fellowship. I work on the intersection of human-computer interaction and generative AI with <a href="https://www.cs.columbia.edu/~chilton/#">Professor Lydia Chilton</a> in the Computational Design Lab.</p>
+
+            <p class="about_text">
+              In my free time, I like to design, write, and dance in New York City. During the third year of my PhD, I danced on the Columbia Dance Team, performing half time at football and basketball games.
+
+            </p>
+            <p class="about_text">
+              I graduated from UC Berkeley with Distinction in Computer Science and Cognitive Science. I also earned a certificate in New Media. During my undergrad, I loved to make things at the Jacobs Institute of Design.
+
+            </p>
+            <br></br>
+            <a class="cv-button" href="VivianLiu_CV_123124.pdf">Curriculum Vitae</a>
+          </div>
+
+          <img height="484px" src="./images/me.jpg" />
+
+        </div>
+
+
+
         <div>
 
-       
+          <br></br>
 
-      <p class="about_text">I'm currently a fifth year Computer Science PhD Student at Columbia University and a student researcher at Google Deepmind. My PhD was supported by a National Science Foundation Graduate Research Fellowship. I work on the intersection of human-computer interaction and generative AI with <a href="https://www.cs.columbia.edu/~chilton/#">Professor Lydia Chilton</a> in the Computational Design Lab.</p>
 
-      <p class="about_text">
-      In my free time, I like to design, write, and dance in New York City. During the third year of my PhD, I danced on the Columbia Dance Team, performing half time at football and basketball games. 
+          <br></br><br></br>
 
-      </p>
-      <p class="about_text">
-      I graduated from UC Berkeley with Distinction in Computer Science and Cognitive Science. I also earned a certificate in New Media. During my undergrad, I loved to make things at the Jacobs Institute of Design.
+          <Grid container className="maker_gallery" rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid item xs={3}>
+              <img class="maker_pic" src="./images/maker/fishbox.png"></img>
 
-      </p>
-      <br></br>
-      <a class="cv-button" href="VivianLiu_CV_123124.pdf">Curriculum Vitae</a>
+
+            </Grid>
+            <Grid item xs={3}>
+
+              <img class="maker_pic" src="./images/maker/hand.png"></img>
+
+
+            </Grid>
+            <Grid item xs={3}>
+              <img class="maker_pic" src="./images/maker/livinghingevase.png"></img>
+
+
+            </Grid>
+            <Grid item xs={3}>
+              <img class="maker_pic" src="./images/cat.png"></img>
+
+
+            </Grid>
+            <Grid item xs={3}>
+              <img class="maker_pic" src="./images/character.png"></img>
+
+
+
+            </Grid>
+            <Grid item xs={3}>
+              <img class="maker_pic" src="./images/maker/CADhand.png"></img>
+
+
+
+            </Grid>
+            <Grid item xs={3}>
+              <img class="maker_pic" src="./images/maker/infinity_mirror.png"></img>
+
+
+
+            </Grid>
+            <Grid item xs={3}>
+              <img class="maker_pic" src="./images/maker/tomandjerry.png"></img>
+              {/* <img width="256px" src="./logomotion_gifs/tennis1.gif"></img> */}
+
+
+            </Grid>
+
+
+          </Grid>
+
+
+
+
+
+
+
+
+
+        </div>
       </div>
 
-      <img height="484px" src="./images/me.jpg"/>
-
-      </div>
-
-    
-    
-    <div>
-   
-      <br></br>
-
-     
-      <br></br><br></br>
-
-      <Grid container className="maker_gallery" rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={3}>
-        <img class="maker_pic" src="./images/maker/fishbox.png"></img>
-
-
-        </Grid>
-        <Grid item xs={3}>
-
-          <img class="maker_pic" src="./images/maker/hand.png"></img>
-
-
-        </Grid>
-        <Grid item xs={3}>
-        <img class="maker_pic" src="./images/maker/livinghingevase.png"></img>
-
-
-        </Grid>
-        <Grid item xs={3}>
-        <img class="maker_pic" src="./images/cat.png"></img>
-
-
-        </Grid>
-        <Grid item xs={3}>
-        <img class="maker_pic" src="./images/character.png"></img>
-
-
-
-        </Grid>
-        <Grid item xs={3}>
-        <img class="maker_pic" src="./images/maker/CADhand.png"></img>
-
-
-
-        </Grid>
-        <Grid item xs={3}>
-        <img class="maker_pic" src="./images/maker/infinity_mirror.png"></img>
-
-
-
-        </Grid>
-        <Grid item xs={3}>
-        <img class="maker_pic" src="./images/maker/tomandjerry.png"></img>
-        {/* <img width="256px" src="./logomotion_gifs/tennis1.gif"></img> */}
-
-
-        </Grid>
-       
-       
-        </Grid>
-
-
-
-    
 
 
 
 
-
-</div>
-</div>
-
-      
-
-
-     
     </div>
   );
 };
