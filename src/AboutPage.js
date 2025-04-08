2c624aa19fb7d7554306f26523bd2cca720500c5
Vivian <v.liu@berkeley.edu>
2025-04-08 12:10:47 -0400

    edited cv

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -30,7 +30,7 @@ const AboutPage = () => {
 
             </p>
             <br></br>
-            <a class="cv-button" href="VivianLiu_CV_123124.pdf">Curriculum Vitae</a>
+            <a class="cv-button" href="VivianLiu_Columbia_CV_Mar_2025.pdf">Curriculum Vitae</a>
           </div>
 
           <img height="484px" src="./images/me.jpg" />
