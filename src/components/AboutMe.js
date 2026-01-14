fc481e20ae99c57888039c58029dd413fce91c09
Vivian <v.liu@berkeley.edu>
2026-01-14 11:53:38 -0800

    adding new position

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,7 @@ const AboutMe = () => {
   return (
     <div className="about-me">
       <h1>Vivian Liu</h1>
-      <p class="about-text">I'm currently a fifth year Computer Science PhD Student at Columbia University. My PhD was supported by a National Science Foundation Graduate Research Fellowship. I work on the intersection of human-computer interaction and generative AI with <a href="https://www.cs.columbia.edu/~chilton/#">Professor Lydia Chilton</a> in the Computational Design Lab.</p>
+      <p class="about-text">I'm currently a sixth year Computer Science PhD Student at Columbia University. I am also a Senior Research Scientist at Autodesk Research. My PhD was supported by a National Science Foundation Graduate Research Fellowship. I work on the intersection of human-computer interaction and generative AI with <a href="https://www.cs.columbia.edu/~chilton/#">Professor Lydia Chilton</a> in the Computational Design Lab.</p>
     </div>
   );
 };
